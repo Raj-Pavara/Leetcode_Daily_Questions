48e3dc6a7d663565e5b930c977a7471edce96948
Raj-Pavara <rajpavara63@gmail.com>
2024-03-29 12:02:58 +0530

    29/03/2024

---
+++ march_29_24.java
@@ -0,0 +1,52 @@
+/*Count Subarrays Where Max Element Appears at Least K Times
+ 
+You are given an integer array nums and a positive integer k.
+
+Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+
+A subarray is a contiguous sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+Example 2:
+
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= k <= 105
+ */
+public class march_29_24{
+public static void main(String args[]){
+   int arr[]= {61,23,38,23,56,40,82,56,82,82,82,70,8,69,8,7,19,14,58,42,82,10,82,78,15,82};
+   System.out.println(countSubarrays(arr, 2));
+}
+public static long countSubarrays(int[] nums, int k) {
+
+    long res = 0;
+        int max = 0, count = 0, n = nums.length;
+        for (int num : nums)
+            max = Math.max(max, num);
+        int l = 0;
+        for (int r = 0; r < n; r++) {
+            if (nums[r] == max)
+                count++;
+            while (count >= k) {
+                res += n - r;
+                if (nums[l] == max)
+                    count--;
+                l++;
+            }
+        }
+        return res;
+    
+}
+}
