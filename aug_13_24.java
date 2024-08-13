db903623d0a438b238e59a1590166bde57be1a0f
Raj-Pavara <rajpavara05@gmail.com>
2024-08-13 17:14:18 +0530

    13/08/2024

---
+++ aug_13_24.java
@@ -0,0 +1,64 @@
+/*40. Combination Sum II
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30 */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class aug_13_24 {
+public static void main(String args[]){
+
+}
+     public List<List<Integer>> combinationSum2(int[] cand, int target) {
+    Arrays.sort(cand);
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<Integer> path = new ArrayList<Integer>();
+    dfs(cand, 0, target, path, res);
+    return res;
+}
+void dfs(int[] cand, int cur, int target, List<Integer> path, List<List<Integer>> res) {
+    if (target == 0) {
+        res.add(new ArrayList(path));
+        return ;
+    }
+    if (target < 0) return;
+    for (int i = cur; i < cand.length; i++){
+        if (i > cur && cand[i] == cand[i-1]) continue;
+        path.add(path.size(), cand[i]);
+        dfs(cand, i+1, target - cand[i], path, res);
+        path.remove(path.size()-1);
+    }
+}
+}
